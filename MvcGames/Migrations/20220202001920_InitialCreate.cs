c7c5e8178604697c234eb94d7220136739676bcc
gusta <gusta@DESKTOP-6DN0JCI>
2022-02-01 18:29:15 -0600

    Agregar archivos de proyecto.

---
+++ MvcGames/Migrations/20220202001920_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcGames.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Clasification = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Size = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Company = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
---
+++ MvcGames/Models/SeedData.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcGames.Data;
+using System;
+using System.Linq;
+
+namespace MvcGames.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcGamesContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcGamesContext>>()))
+            {
+                // Look for any games.
+                if (context.Games.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Games.AddRange(
+                    new Games
+                    {
+                        Title = "Horizon Zero Dawn",
+                        ReleaseDate = DateTime.Parse("2070-2-28"),
+                        Genre = "RPG",
+                        Price = 38.99M,
+                        Clasification = "16",
+                        Size = "33.22 GB",
+                        Company = "Guerrilla Games"
+                    },
+
+                    new Games
+                    {
+                        Title = "God of war",
+                        ReleaseDate = DateTime.Parse("2018-4-20"),
+                        Genre = "RPG Hack and Slash",
+                        Price = 20.00M,
+                        Clasification = "18",
+                        Size = "28.80 GB",
+                        Company = "Sony Interactive Entertainment"
+                    },
+
+                    new Games
+                    {
+                        Title = "Nioh",
+                        ReleaseDate = DateTime.Parse("2017-2-7"),
+                        Genre = "Hack and Slash",
+                        Price = 18.99M,
+                        Clasification = "18",
+                        Size = "27.07 GB",
+                        Company = "Team Ninja"
+                    },
+
+                    new Games
+                    {
+                        Title = "Control",
+                        ReleaseDate = DateTime.Parse("2019-8-27"),
+                        Genre = "Shooter",
+                        Price = 11.99M,
+                        Clasification = "18",
+                        Size = "32.90 GB",
+                        Company = "Remedy Entertainment"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
